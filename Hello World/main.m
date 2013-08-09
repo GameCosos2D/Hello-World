1074bbc903fae46102ea7922f819d98dbfee020e
Marco Velluto <velluto93@gmail.com>
2013-08-09 23:48:33 +0200

    Initial Commit

---
+++ Hello World/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Marco Velluto on 09/08/13.
+//  Copyright Algos 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
